4aed53e62d24cc7f2ac75bfe69167832f18b8b9a
rosta999 <43803039+rosta999@users.noreply.github.com>
2018-11-22 14:23:38 -0800

    Update vm.c

--- vm.c
+++ vm.c
@@ -313,7 +313,7 @@ clearpteu(pde_t *pgdir, char *uva)
 // Given a parent process's page table, create a copy
 // of it for a child.
 pde_t*
-copyuvm(pde_t *pgdir, uint sz)
+copyuvm(pde_t *pgdir, uint st)
 {
   pde_t *d;
   pte_t *pte;
@@ -322,7 +322,7 @@ copyuvm(pde_t *pgdir, uint sz)
 
   if((d = setupkvm()) == 0)
     return 0;
-  for(i = 0; i < sz; i += PGSIZE){
+  for(i = st - PGSIZE; i < st; i += PGSIZE){
     if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
       panic("copyuvm: pte should exist");
     if(!(*pte & PTE_P))
